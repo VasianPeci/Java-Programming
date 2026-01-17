6176e1cc83d4536d0644bb98d9cebe39f8854d35
VasianPeci <140719484+VasianPeci@users.noreply.github.com>
2026-01-17 20:17:33 +0100

    Weighted Quick Union Algorithm

--- UF.java
+++ UF.java
@@ -1,38 +1,39 @@
-public class UF {
-    private int[] id; // access to component id (site-indexed)
+public class UF
+{
+    private int[] id; // parent link (site indexed)
+    private int[] sz; // size of component for roots (site indexed)
     private int count; // number of components
 
-    public UF (int N) {
-        // Initialize component id array
+    public UF(int N)
+    {
         count = N;
         id = new int[N];
-        for (int i = 0; i < N; i++) {
-            id[i] = i;
-        }
+        for (int i = 0; i < N; i++) id[i] = i;
+        sz = new int[N];
+        for (int i = 0; i < N; i++) sz[i] = 1;
     }
-
-    public int count () {
-        return count;
-    }
-
-    private boolean connected (int p, int q) {
-        return find(p) == find(q);
-    }
-
-    private int find (int p) {
-        // Find component name.
+    
+    public int count()
+    {  return count;  }
+    
+    public boolean connected(int p, int q)
+    {  return find(p) == find(q);  }
+    
+    private int find(int p)
+    { // Follow links to find a root.
         while (p != id[p]) p = id[p];
         return p;
     }
-
-    public void union (int p, int q) {
-        // Give p and q the same root.
-        int pRoot = find(p);
-        int qRoot = find(q);
-        if (pRoot == qRoot) return;
-
-        id[pRoot] = qRoot;
-
+    
+    public void union(int p, int q)
+    {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) return;
+        
+        // Make smaller root point to larger one.
+        if   (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
+        else                 { id[j] = i; sz[i] += sz[j]; }
         count--;
     }
 }
